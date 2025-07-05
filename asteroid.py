0903951628ca8b008e49ad5530156782f2e16981
penguinpencat <melanacaffullfreear@gmail.com>
2025-07-05 14:32:42 +0100

    I: Created the Asteroid Class

---
+++ asteroid.py
@@ -0,0 +1,19 @@
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            surface=screen, 
+            color=(255, 255, 255), 
+            center=self.position,
+            radius=self.radius,
+            width=2
+        )
+    
+    def update(self, dt):
+        # Moving forward
+        self.position += self.velocity * dt
